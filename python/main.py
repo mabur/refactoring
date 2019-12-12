3b411866339b9688caa87669484f935db05ef2ef
mabur <magnus.burenius@volumental.com>
2019-12-12 16:43:50 +0100

    Add symbols for similar refactorings in generated svg

--- python/main.py
+++ python/main.py
@@ -1,5 +1,20 @@
 import svgwrite
 
+MOVE = '-->'
+RENAME = 'Abc'
+EXTRACT_INLINE = '...'
+REMOVE_ADD = '+ -'
+VISIBILITY = '( )'
+STATE_SPACE = '\u00A0!'
+CONTROL = 'JMP'
+
+SPECIAL_STRINGS = [MOVE, RENAME, EXTRACT_INLINE, REMOVE_ADD, VISIBILITY, STATE_SPACE, CONTROL]
+
+def strip_special_strings(s: str) -> str:
+    for special_string in SPECIAL_STRINGS:
+        s = s.replace(special_string, '')
+    return s
+
 content = {
     #'Symbols': [
     #    'Rename',
@@ -10,75 +25,76 @@ content = {
     #    'Re-use / duplicate',
     #],
     'Variables': [
-        'Move variable',
-        'Rename variable',
-        'Extract variable / Inline variable',
-        'Remove unused variable / Add unused variable',
-        'Public variable / Private variable',
-        'Check null pointer',
-        'Check array bounds',
+        'Move variable' + MOVE,
+        'Rename variable' + RENAME,
+        'Extract variable / Inline variable' + EXTRACT_INLINE,
+        'Remove unused variable / Add unused variable' + REMOVE_ADD,
+        'Public variable / Private variable' + VISIBILITY,
+        'Check null pointer' + STATE_SPACE,
+        'Check array bounds' + STATE_SPACE,
+        'Check invariance' + STATE_SPACE,
     ],
     'Functions': [
-        'Move function',
-        'Rename function',
-        'Extract function / Inline function',
-        'Remove unused function / Add unused function',
-        'Public function / Private function',
+        'Move function' + MOVE,
+        'Rename function' + RENAME,
+        'Extract function / Inline function' + EXTRACT_INLINE,
+        'Remove unused function / Add unused function' + REMOVE_ADD,
+        'Public function / Private function' + VISIBILITY,
         'Move code outside of function / Move code inside of function',
         'Separate query and modifier',
-        'Explicit loop / Recursion',
-        'Nested conditionals / Guard clauses',
-        'Encapsulate mutable variable',
-        'Remove mutable control flag',
-        'Errors as return values / Exceptions',
-        'Check pre-condition',
-        'Check post-condition',
+        'Explicit loop / Recursion' + CONTROL,
+        'Nested conditionals / Guard clauses' + CONTROL,
+        'Errors as return values / Exceptions' + CONTROL,
+        'Remove mutable control flag' + CONTROL,
+        'Encapsulate mutable variable' + STATE_SPACE,
+        'Check pre-condition' + STATE_SPACE,
+        'Check post-condition' + STATE_SPACE,
         #'Replace parameter with function / Parameterize function',
     ],
     'Types': [
-        'Move type',
-        'Rename type',
-        'Extract type / Inline type',
-        'Remove unused type / Add unused type',
-        'Public type / Private type',
-        'Mutable / Immutable',
+        'Move type' + MOVE,
+        'Rename type' + RENAME,
+        'Extract type / Inline type' + EXTRACT_INLINE,
+        'Remove unused type / Add unused type' + REMOVE_ADD,
+        'Public type / Private type' + VISIBILITY,
         'Direct member reading / Getter function',
         'Direct member writing / Setter function',
-        'Scope based resource management / Manual resource management',
+        'Scope based resource management / Manual resource management' + CONTROL,
         'Add constructor / Remove constructor',
         'Add destructor / Remove destructor',
         'Constructor / Factory function',
-        'Categorical constants / Enums',
-        'Reduce state space',
+        'Mutable / Immutable' + STATE_SPACE,
+        'Categorical constants / Enums' + STATE_SPACE,
+        'Reduce state space' + STATE_SPACE,
         #'Hide delegate /remove delegate',
     ],
     'Polymorphism': [
-        'Move interface',
-        'Rename interface',
-        'Extract interface / Inline interface',
-        'Remove unused interface / Add unused interface',
-        'Public interface / Private interface',
-        'Custom type / Generic collection',
-        'Custom loop / Generic algorithm',
+        'Move interface' + MOVE,
+        'Rename interface' + RENAME,
+        'Extract interface / Inline interface' + EXTRACT_INLINE,
+        'Remove unused interface / Add unused interface' + REMOVE_ADD,
+        'Public interface / Private interface' + VISIBILITY,
         'Inheritance / Composition',
-        'Switch cases or conditionals / Polymorphism',
-        'Conditionals for special case / Add null object',
+        'Custom type / Generic collection',
+        'Custom loop / Generic algorithm' + CONTROL,
+        'Switch cases or conditionals / Polymorphism' + CONTROL,
+        'Conditionals for special case / Add null object' + CONTROL,
         #'Replace subclass with fields',
         #'Add unused factory function',
     ],
     'Files': [
-        'Move file',
-        'Rename file',
-        'Extract file / Inline file',
-        'Remove unused file / Add unused file',
-        'Visible file / Hidden file',
+        'Move file' + MOVE,
+        'Rename file' + RENAME,
+        'Extract file / Inline file' + EXTRACT_INLINE,
+        'Remove unused file / Add unused file' + REMOVE_ADD,
+        'Visible file / Hidden file' + VISIBILITY,
     ],
     'Directories': [
-        'Move directory',
-        'Rename directory',
-        'Extract directory / Inline directory',
-        'Remove unused directory / Add unused directory',
-        'Visible directory / Hidden directory',
+        'Move directory' + MOVE,
+        'Rename directory' + RENAME,
+        'Extract directory / Inline directory' + EXTRACT_INLINE,
+        'Remove unused directory / Add unused directory' + REMOVE_ADD,
+        'Visible directory / Hidden directory' + VISIBILITY,
     ]
 }
 
@@ -172,7 +188,7 @@ Y_START = 100
 TEXT_OFFSET_X = 20
 TEXT_OFFSET_Y = -DY + 22
 CIRCLE_OFFSET_X = DX - 20
-CIRCLE_OFFSET_Y = -DY / 2 #-DY + 20
+CIRCLE_OFFSET_Y = -DY + 20 #-DY + 20
 DIVIDER = 0
 ARROW_OFFSET_X = +DX * 0.2
 ARROW_OFFSET_Y = -DY * 0.5
@@ -204,7 +220,12 @@ for category_name, refactorings in content.items():
         _draw_line_horizontal(drawing, x=x, y=y - DY / 2, w=DIVIDER)
         _draw_line_horizontal(drawing, x=x + DX - DIVIDER, y=y - DY / 2, w=DIVIDER)
 
-        lines = refactoring.split('/')
+        lines = [line for line in strip_special_strings(refactoring).split('/')]
+
+        for special_string in SPECIAL_STRINGS:
+            if special_string in refactoring:
+                _draw_text(drawing, special_string, x=x + CIRCLE_OFFSET_X - 16, y=y + TEXT_OFFSET_Y + DY_LINE, **FONT)
+
         if len(lines) < 2:
             _draw_text(drawing, lines[0], x=x + TEXT_OFFSET_X, y=y + TEXT_OFFSET_Y + DY_LINE / 2, **FONT)
         else:
