72fe38300034b9f20345a287872b688b182f064f
mabur <magnus.burenius@volumental.com>
2019-12-12 15:17:07 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -4,82 +4,81 @@ content = {
     #'Symbols': [
     #    'Rename',
     #    'Extract / Inline',
-    #    'Remove / Add',
+    #    'Remove unused / Add',
     #    'Move',
     #    'Hide',
     #    'Re-use / duplicate',
     #],
     'Variables': [
+        'Move variable',
         'Rename variable',
         'Extract variable / Inline variable',
-        'Remove variable / Add variable',
-        'Move variable',
-        'Hide variable',
-        'Re-use variable / Duplicate variable',
+        'Remove unused variable / Add unused variable',
+        'Public variable / Private variable',
+        'Check null pointer',
+        'Check array bounds',
     ],
     'Functions': [
+        'Move function',
         'Rename function',
         'Extract function / Inline function',
-        'Remove function / Add function',
-        'Move function',
-        'Hide function',
-        'Re-use function / Duplicate function',
-        'Encapsulate mutable variable',
+        'Remove unused function / Add unused function',
+        'Public function / Private function',
+        'Move code outside of function / Move code inside of function',
         'Separate query and modifier',
-        'Replace parameter with function / Parameterize function',
+        'Explicit loop / Recursion',
+        'Nested conditionals / Guard clauses',
         'Encapsulate mutable variable',
         'Remove mutable control flag',
-        'Nested conditionals / Guard clauses',
-        'Move fragment to caller/called',
-        'Replace loop with recursion',
+        'Errors as return values / Exceptions',
+        'Check pre-condition',
+        'Check post-condition',
+        #'Replace parameter with function / Parameterize function',
     ],
     'Types': [
+        'Move type',
         'Rename type',
         'Extract type / Inline type',
-        'Remove type / Add type',
-        'Move type',
-        'Hide type',
-        'Re-use type / Duplicate type',
-        'Make type immutable / Make type mutable',
-        'Get data via function',
-        'Set data via function',
-        'Hide/remove delegate',
-        'Add/remove constructor',
-        'Add/remove destructor',
-        'Reduce state space',
+        'Remove unused type / Add unused type',
+        'Public type / Private type',
+        'Mutable / Immutable',
+        'Direct member reading / Getter function',
+        'Direct member writing / Setter function',
+        'Scope based resource management / Manual resource management',
+        'Add constructor / Remove constructor',
+        'Add destructor / Remove destructor',
         'Constructor / Factory function',
-        'Replace integer/string with enum',
+        'Categorical constants / Enums',
+        'Reduce state space',
+        #'Hide delegate /remove delegate',
     ],
     'Polymorphism': [
+        'Move interface',
         'Rename interface',
         'Extract interface / Inline interface',
-        'Remove interface / Add interface',
-        'Move interface',
-        'Hide interface',
-        'Re-use interface / Duplicate interface',
-        'Inheritance / Composition',
-        'Add factory function',
-        'Conditionals and type codes / Polymorphism',
-        'Replace subclass with fields',
-        'Add null object / special case',
+        'Remove unused interface / Add unused interface',
+        'Public interface / Private interface',
         'Custom type / Generic collection',
         'Custom loop / Generic algorithm',
+        'Inheritance / Composition',
+        'Switch cases or conditionals / Polymorphism',
+        'Conditionals for special case / Add null object',
+        #'Replace subclass with fields',
+        #'Add unused factory function',
     ],
     'Files': [
+        'Move file',
         'Rename file',
         'Extract file / Inline file',
-        'Remove / Add file',
-        'Move file',
-        'Hide file',
-        'Re-use file / Duplicate file',
+        'Remove unused file / Add unused file',
+        'Visible file / Hidden file',
     ],
     'Directories': [
+        'Move directory',
         'Rename directory',
         'Extract directory / Inline directory',
-        'Remove / Add directory',
-        'Move directory',
-        'Hide directory',
-        'Re-use directory / Duplicate directory',
+        'Remove unused directory / Add unused directory',
+        'Visible directory / Hidden directory',
     ]
 }
 
@@ -140,47 +139,63 @@ def _draw_arrows(drawing, x, y, h, a):
     _draw_line(drawing, x0=x, y0=y + h / 2, x1=x - a, y1=y + h / 2 - a)
 
 
-FONT_NAME = 'Oxygen Mono'#'Raleway'#'Oxygen Mono'
+FONT_CAPTION = {
+    'font_size': '2.0em',
+    'font-family': 'Oxygen Mono', #'Merriweather', #'Playfair Display',#'Merriweather',
+    'fill': 'black',
+    #'font_weight': 'bold',
+}
 
 FONT = {
-    'font_size': '0.8em',
-    'font-family': FONT_NAME,
+    'font_size': '1.0em',
+    'font-family': 'Oxygen Mono',#'Raleway'#'Oxygen Mono'
     'fill': 'black',
 }
+
+FONTS = [FONT, FONT_CAPTION]
 
 COLOR_FROM_CATEGORY = {
     'Variables': '#42bdc5',
     'Functions': '#fccc8b',
-    'Types': '#99d5e2',
+    'Types': '#7cc161',
     'Polymorphism': '#ef7f8e',
     'Files': '#6674b6',
     'Directories': '#1f2547',
 }
 
-DX = 300
+DX = 350
 DY = 80
-DY_LINE = 40
+DY_LINE = 45
 X_START = 30
 Y_START = 100
 TEXT_OFFSET_X = 20
-TEXT_OFFSET_Y = -DY + 20
+TEXT_OFFSET_Y = -DY + 22
 CIRCLE_OFFSET_X = DX - 20
-CIRCLE_OFFSET_Y = -DY + 20
+CIRCLE_OFFSET_Y = -DY / 2 #-DY + 20
 DIVIDER = 0
+ARROW_OFFSET_X = +DX * 0.2
+ARROW_OFFSET_Y = -DY * 0.5
+
+CAPTION_OFFSET_X = 20
+CAPTION_OFFSET_Y = -DY + 45
+
+RADIUS = 7
 
 x = X_START
 last_height = 0
 current_height = 0
 drawing = svgwrite.Drawing('refactorings.svg')
 
-drawing.embed_google_web_font(name=FONT_NAME, uri="https://fonts.googleapis.com/css?family={}".format(FONT_NAME.replace(' ', '+')))
+for font in FONTS:
+    name = font['font-family']
+    drawing.embed_google_web_font(name=name, uri="https://fonts.googleapis.com/css?family={}".format(name.replace(' ', '+')))
 
 for category_name, refactorings in content.items():
     y = Y_START
     current_height = DY * len(refactorings)
     _draw_line_vertical(drawing, x=x, y=Y_START, h=max(current_height, last_height))
     last_height = current_height
-    _draw_text(drawing, category_name.upper(), x + TEXT_OFFSET_X, y + TEXT_OFFSET_Y, **FONT, font_weight='bold')
+    _draw_text(drawing, category_name, x + CAPTION_OFFSET_X, y + CAPTION_OFFSET_Y, **FONT_CAPTION)
     _draw_line_horizontal(drawing, x=x, y=y, w=DX)
     y += DY
     for refactoring in refactorings:
@@ -189,12 +204,14 @@ for category_name, refactorings in content.items():
         _draw_line_horizontal(drawing, x=x + DX - DIVIDER, y=y - DY / 2, w=DIVIDER)
 
         lines = refactoring.split('/')
-        _draw_text(drawing, lines[0], x=x + TEXT_OFFSET_X, y=y + TEXT_OFFSET_Y, **FONT)
-        if len(lines) > 1:
+        if len(lines) < 2:
+            _draw_text(drawing, lines[0], x=x + TEXT_OFFSET_X, y=y + TEXT_OFFSET_Y + DY_LINE / 2, **FONT)
+        else:
+            _draw_text(drawing, lines[0], x=x + TEXT_OFFSET_X, y=y + TEXT_OFFSET_Y, **FONT)
             _draw_text(drawing, lines[1], x=x + TEXT_OFFSET_X, y=y + TEXT_OFFSET_Y + DY_LINE, **FONT)
-            _draw_arrows(drawing, x=x + DX / 2, y=y - DY / 2, h=14, a=4)
+            _draw_arrows(drawing, x=x + ARROW_OFFSET_X, y=y + ARROW_OFFSET_Y, h=14, a=4)
 
-        _draw_circle(drawing, x=x + CIRCLE_OFFSET_X, y=y + CIRCLE_OFFSET_Y, r=5, c=COLOR_FROM_CATEGORY[category_name])
+        _draw_circle(drawing, x=x + CIRCLE_OFFSET_X, y=y + CIRCLE_OFFSET_Y, r=RADIUS, c=COLOR_FROM_CATEGORY[category_name])
         y += DY
     x += DX
 _draw_line_vertical(drawing, x=x, y=Y_START, h=max(current_height, last_height))
